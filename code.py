694ac8811197acb5a781e979674f289853de6838
Santhosh Ande <andesantosh123@gmail.com>
2024-01-09 13:26:53 -0600

    commiting to check the code

---
+++ code.py
@@ -0,0 +1,5 @@
+# Addition of two numbers
+num1 = float(input("enter the first number for addition: "))
+num2 = float(input("enter the second number for addition: "))
+sum_result = num1 + num2
+print(f" sum: {num1} + {num2} = {sum_result}")
